203a8eb722afb4b2d6b084b8a43e6621a7590a8c
martinalee94 <martinalee0528@gmail.com>
2023-07-13 05:52:03 +0900

    #4 Lesson 4
    - Request and response

--- server.js
+++ server.js
@@ -1,9 +1,61 @@
 const http = require('http');
-
+const fs = require('fs');
 
 // ex. app = FastAPI()
 const server = http.createServer((req, res) => {
     console.log('request made');
+    console.log(req.url, req.method);
+
+    // set header content type
+    // res.setHeader('Content-Type', 'text/plain')
+    // res.write('hello, ninjas');
+
+    // res.setHeader('Content-Type', 'text/html')
+    // res.write('<p>hello, ninjas</p>')
+
+    // send an html file
+    res.setHeader('Content-Type', 'text/html')
+    // fs.readFile('./views/index.html', (err, data) => {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         res.write(data);
+    //         //res.end(data) this also works
+    //     }
+    //     res.end();
+    // });
+
+    let path = './views/';
+    switch (req.url) {
+        case '/':
+            res.statusCode = 200;
+            path += 'index.html';
+            break;
+        case '/about':
+            res.statusCode = 200;
+            path += 'about.html';
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            res.statusCode = 404;
+            path += '404.html';
+            break;
+    }
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.write(data);
+            //res.end(data) this also works
+        }
+        res.end();
+    });
+
+
 });
 
 server.listen(3000, 'localhost', () => {
