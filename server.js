a2c3343ccbefaa19a23689086c6eba461a7234cf
martinalee94 <martinalee0528@gmail.com>
2023-07-13 07:29:14 +0900

    #5 Lesson 5
    - NPM, install third-party packages

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http = require('http');
 const fs = require('fs');
-
+const _ = require('lodash');
 // ex. app = FastAPI()
 const server = http.createServer((req, res) => {
     console.log('request made');
     console.log(req.url, req.method);
+    // lodash
+    const num = _.random(0, 20);
+    console.log(num)
+
+    // Run only once
+    const greet = _.once(() => {
+        console.log('hello');
+    });
 
     // set header content type
     // res.setHeader('Content-Type', 'text/plain')
