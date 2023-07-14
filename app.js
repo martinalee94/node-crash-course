186d0f547746bd9da0be15c5a59444e79b66bd29
Martina Lee <39133877+martinalee94@users.noreply.github.com>
2023-07-15 06:51:29 +0900

    #7 View engines - ejs (#2)

--- app.js
+++ app.js
@@ -3,31 +3,36 @@ const express = require('express');
 // Set up express app
 const app = express();
 
+// Register view engine
+app.set('view engine', 'ejs');
 
+// Change view directory(default is 'views')
+// app.set('views', 'myviews');
 
 // Listen for requests
 app.listen(3000);
 
 
 app.get('/', (req, res) => {
-    // res.send('<p>Home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        { title: "Yoshi finds eggs", snippet: 'Lorem ipsum dolor' },
+        { title: "Mario finds stars", snippet: 'Lorem ipsum dolor' }
+    ]
+    res.render('index', { title: 'Home', blogs });
 })
 
 app.get('/about', (req, res) => {
-    // res.send('<p>About page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
-
+    res.render('about');
 })
 
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+
 })
 
 // 404
 // Use this function for every incoming request
 // 위에 다 지나고 내려와서 여기까지 오면 해당 함수 실행하는 것
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 })
