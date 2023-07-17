034747f8baf1836df0ae3429963ad26e807e2a66
martinalee94 <martinalee0528@gmail.com>
2023-07-18 07:50:54 +0900

    #9 Lesson 9 - MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
 // Set up express app
 const app = express();
+
+// Connect to DB
+require('dotenv').config()
+
+const dbURI = `mongodb+srv://${process.env.USER_ID}:${process.env.PASSWORD}@cluster0.bilp9sw.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(dbURI).then((res) => {
+    console.log('connected to db');
+    // Listen for requests
+    app.listen(3000);
+}).catch((err) => {
+    console.log(err);
+});
 
 // Register view engine
 app.set('view engine', 'ejs');
@@ -9,40 +24,75 @@ app.set('view engine', 'ejs');
 // Change view directory(default is 'views')
 // app.set('views', 'myviews');
 
-// Listen for requests
-app.listen(3000);
 
-// middleware 1
-// app.use((req, res, next) => {
-//     console.log('new request made: ');
-//     console.log('host: ', req.hostname);
-//     console.log('path: ', req.path);
-//     console.log('method: ', req.method);
-//     next(); // move on to the next
-// })
+/*
+middleware 1
+app.use((req, res, next) => {
+    console.log('new request made: ');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next(); // move on to the next
+})
 
-// middleware 2
-// app.use((req, res, next) => {
-//     console.log('In the next middleware');
-//     next(); // move on to the next
-// })
+middleware 2
+app.use((req, res, next) => {
+    console.log('In the next middleware');
+    next(); // move on to the next
+})
+*/
 
 // static files middleware
 app.use(express.static('public')); // static file folder
 // logging middleware
 app.use(morgan('dev'));
 
+// mongoose and mongo sandbox routes
+/*
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({ title: "Mario finds stars", snippet: 'Lorem ipsum dolor', body: 'Lorem ipsum dolor' });
+    blog.save().then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        console.log(err);
+    });
+})
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find().then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        console.log(err);
+    })
+})
+
+app.get('/single-blog', (req, res) => {
+    Blog.findById(req.id).then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        console.log(err);
+    })
+})
+*/
 
 app.get('/', (req, res) => {
-    const blogs = [
-        { title: "Yoshi finds eggs", snippet: 'Lorem ipsum dolor' },
-        { title: "Mario finds stars", snippet: 'Lorem ipsum dolor' }
-    ]
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs');
 })
 
 app.get('/about', (req, res) => {
     res.render('about');
+})
+
+// -------blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 }) // descending order
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs: result });
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+
 })
 
 app.get('/blogs/create', (req, res) => {
